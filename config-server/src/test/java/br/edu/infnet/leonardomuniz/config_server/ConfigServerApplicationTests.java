f7db4291073ce3f451b5463f1f3188ef5495fd5e
Leonardo Muniz <leonardocmuniz@icloud.com>
2026-08-17 19:40:56 -0300

    feat(config-server): initialize Spring Cloud Config Server

---
+++ config-server/src/test/java/br/edu/infnet/leonardomuniz/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.leonardomuniz.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
