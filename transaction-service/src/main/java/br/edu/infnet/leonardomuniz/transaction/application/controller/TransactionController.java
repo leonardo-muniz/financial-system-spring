c100d1ea41d5549e48e52066a1ade4565434b8a1
Leonardo Muniz <leonardocmuniz@icloud.com>
2026-08-21 20:42:16 -0300

    fix(transaction-service): update controller to match new createForAccount signature

--- transaction-service/src/main/java/br/edu/infnet/leonardomuniz/transaction/application/controller/TransactionController.java
+++ transaction-service/src/main/java/br/edu/infnet/leonardomuniz/transaction/application/controller/TransactionController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.infnet.leonardomuniz.transaction.application.dto.TransactionResponse;
+import br.edu.infnet.leonardomuniz.transaction.domain.model.TransactionType;
 import br.edu.infnet.leonardomuniz.transaction.domain.service.TransactionService;
 
 @RestController
@@ -22,8 +24,9 @@ public class TransactionController {
     @PostMapping("/{accountId}")
     public TransactionResponse create(
             @PathVariable Long accountId,
-            @RequestBody Double amount
+            @RequestBody Double amount,
+            @RequestParam TransactionType type
     ) { 
-        return service.createForAccount(accountId, amount);
+        return service.createForAccount(accountId, amount, type);
     }
 }
