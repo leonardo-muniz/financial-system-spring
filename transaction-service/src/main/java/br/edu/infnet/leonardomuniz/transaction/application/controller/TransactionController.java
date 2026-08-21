1f80035bc6a7594979c5117c65c9859295f8c5b8
Leonardo Muniz <leonardocmuniz@icloud.com>
2026-08-21 19:01:17 -0300

    refactor(transaction-service): simplify controller to delegate transaction creation entirely to service layer

--- transaction-service/src/main/java/br/edu/infnet/leonardomuniz/transaction/application/controller/TransactionController.java
+++ transaction-service/src/main/java/br/edu/infnet/leonardomuniz/transaction/application/controller/TransactionController.java
@@ -1,16 +1,13 @@
 package br.edu.infnet.leonardomuniz.transaction.application.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.infnet.leonardomuniz.transaction.application.dto.TransactionResponse;
-import br.edu.infnet.leonardomuniz.transaction.domain.model.Transaction;
 import br.edu.infnet.leonardomuniz.transaction.domain.service.TransactionService;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("api/transactions")
@@ -26,14 +23,7 @@ public class TransactionController {
     public TransactionResponse create(
             @PathVariable Long accountId,
             @RequestBody Double amount
-    ) {
-        Transaction tx = service.createForAccount(accountId, amount);
-        
-        return TransactionResponse.builder()
-                .transactionId(tx.getId())
-                .accountId(tx.getAccountId())
-                .amount(tx.getAmount())
-                .status(tx.getStatus())
-                .build();
+    ) { 
+        return service.createForAccount(accountId, amount);
     }
 }
