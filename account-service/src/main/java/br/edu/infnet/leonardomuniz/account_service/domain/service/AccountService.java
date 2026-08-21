bb61973a07714510b6a6ac669b2fd44ba49728c8
Leonardo Muniz <leonardocmuniz@icloud.com>
2026-08-21 18:32:26 -0300

    feat(account-service): enhance domain service with account existence validation and initial in-memory data store

--- account-service/src/main/java/br/edu/infnet/leonardomuniz/account_service/domain/service/AccountService.java
+++ account-service/src/main/java/br/edu/infnet/leonardomuniz/account_service/domain/service/AccountService.java
@@ -1,16 +1,24 @@
 package br.edu.infnet.leonardomuniz.account_service.domain.service;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 import br.edu.infnet.leonardomuniz.account_service.domain.model.Account;
+import br.edu.infnet.leonardomuniz.account_service.domain.exception.AccountNotFoundException;
 
 @Service
 public class AccountService {
-    public Account getById(Long id) {
-        return Account.builder()
-                .id(id)
-                .ownerName("Leonardo")
-                .balance(1000.0)
-                .build();
+
+    private final Map<Long, Account> fakeDb = Map.of(
+            1L, Account.builder().id(1L).ownerName("Leonardo").balance(1000.0).build(),
+            2L, Account.builder().id(2L).ownerName("Maria").balance(500.0).build()
+    );
+
+    public Account getById(Long id) { return fakeDb.getOrDefault(id, null); }
+
+    public void validateAccountExists(Long id) {
+        if (!fakeDb.containsKey(id)) throw new AccountNotFoundException(id);
     }
+    
 }
