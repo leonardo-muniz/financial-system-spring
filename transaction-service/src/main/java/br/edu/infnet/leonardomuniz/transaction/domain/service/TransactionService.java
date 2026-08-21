d71f0958f25aab4f521a4610e0693e6b1136ff28
Leonardo Muniz <leonardocmuniz@icloud.com>
2026-08-21 18:59:20 -0300

    refactor(transaction-service): move response assembly to domain service and return TransactionResponse directly

--- transaction-service/src/main/java/br/edu/infnet/leonardomuniz/transaction/domain/service/TransactionService.java
+++ transaction-service/src/main/java/br/edu/infnet/leonardomuniz/transaction/domain/service/TransactionService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import br.edu.infnet.leonardomuniz.transaction.application.dto.AccountDto;
+import br.edu.infnet.leonardomuniz.transaction.application.dto.TransactionResponse;
 import br.edu.infnet.leonardomuniz.transaction.domain.exception.InsufficientBalanceException;
 import br.edu.infnet.leonardomuniz.transaction.domain.model.Transaction;
 import br.edu.infnet.leonardomuniz.transaction.infrastructure.client.AccountClient;
@@ -18,17 +19,25 @@ public class TransactionService {
         this.accountClient = accountClient;
     }
 
-    public Transaction createForAccount(Long accountId, Double amount) {
+    public TransactionResponse createForAccount(Long accountId, Double amount) {
 
         AccountDto account = accountClient.getAccount(accountId);
 
         if (account.getBalance() < amount) throw new InsufficientBalanceException(account.getBalance(), amount);
 
-        return Transaction.builder()
-                .id(UUID.randomUUID().toString())
-                .accountId(account.getId())
-                .amount(amount)
-                .status("SUCCESS")
+        Transaction tx = Transaction.builder()
+            .id(UUID.randomUUID().toString())
+            .accountId(accountId)
+            .amount(amount)
+            .status("SUCCESS")
+            .build();
+
+        return TransactionResponse.builder()
+                .transactionId(tx.getId())
+                .accountId(tx.getAccountId())
+                .amount(tx.getAmount())
+                .status(tx.getStatus())
+                .account(account)
                 .build();
     }
 }
