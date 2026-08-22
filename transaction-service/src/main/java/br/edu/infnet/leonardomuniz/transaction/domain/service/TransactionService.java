9f652871deb28ae1ef503c7a6902dff25e3c6d6c
Leonardo Muniz <leonardocmuniz@icloud.com>
2026-08-21 21:58:08 -0300

    feat(transaction-service): add validation for debit/credit transaction types

--- transaction-service/src/main/java/br/edu/infnet/leonardomuniz/transaction/domain/service/TransactionService.java
+++ transaction-service/src/main/java/br/edu/infnet/leonardomuniz/transaction/domain/service/TransactionService.java
@@ -9,6 +9,7 @@ import br.edu.infnet.leonardomuniz.transaction.application.dto.TransactionRespon
 import br.edu.infnet.leonardomuniz.transaction.domain.exception.AccountBlockedException;
 import br.edu.infnet.leonardomuniz.transaction.domain.exception.AccountClosedException;
 import br.edu.infnet.leonardomuniz.transaction.domain.exception.InsufficientBalanceException;
+import br.edu.infnet.leonardomuniz.transaction.domain.exception.InvalidTransactionTypeException;
 import br.edu.infnet.leonardomuniz.transaction.domain.model.Transaction;
 import br.edu.infnet.leonardomuniz.transaction.domain.model.TransactionType;
 import br.edu.infnet.leonardomuniz.transaction.infrastructure.client.AccountClient;
@@ -31,6 +32,7 @@ public class TransactionService {
         AccountDto account = accountClient.getAccount(accountId);
 
         validateAccountStatus(account);
+        validateTransactionType(type);
 
         if (account.getBalance() < amount) throw new InsufficientBalanceException(account.getBalance(), amount);
 
@@ -59,6 +61,11 @@ public class TransactionService {
         if ("CLOSED".equalsIgnoreCase(account.getStatus()))
             throw new AccountClosedException(account.getId());
 
+    }
+
+    private void validateTransactionType(TransactionType type) {
+        if (type == null)
+            throw new InvalidTransactionTypeException("null");
     }
 
     // Fallback chamado quando o Circuit Breaker abre ou o Retry esgota
